e4465c34d97c480afe3728a9f0f27aa3a81b6e07
jagrutijadeja <jagrutijadeja842@gmail.com>
2024-07-26 21:07:12 +0100

    virgin-api-testproject

---
+++ src/main/java/com/virgingames/constants/Path.java
@@ -0,0 +1,9 @@
+package com.virgingames.constants;
+
+public class Path {
+
+    /*
+     * This is base path for currency
+     */
+    public static final String Currency= "api/jackpots/bingo?";
+}
